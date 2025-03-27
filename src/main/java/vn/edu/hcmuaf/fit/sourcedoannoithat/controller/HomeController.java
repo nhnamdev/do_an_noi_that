3f0762bb60ed5f51cc2ccc93f967b4c7f77309d2
nam <22130173@st.hcmuaf.edu.vn>
2025-03-27 22:15:54 +0700

    create error db page

--- src/main/java/vn/edu/hcmuaf/fit/sourcedoannoithat/controller/HomeController.java
+++ src/main/java/vn/edu/hcmuaf/fit/sourcedoannoithat/controller/HomeController.java
@@ -3,15 +3,19 @@ package vn.edu.hcmuaf.fit.sourcedoannoithat.controller;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.sourcedoannoithat.dao.db.DBConnect;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 @WebServlet(name = "HomeController", value = "/index")
 public class HomeController extends HttpServlet {
+    DBConnect db = new DBConnect();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+
+
     }
 
     @Override
---
+++ src/main/webapp/errorDb.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>502 Bad Gateway</title>
+
+</head>
+<body>
+<div class="container">
+    <h1>502 Database Error</h1>
+    <p>Xin lỗi nha! Cấu hình lại Database mới truy cập được nhé.</p>
+    <p><a href="${pageContext.request.contextPath}/index">Quay lại trang chủ</a></p>
+</div>
+</body>
+</html>
