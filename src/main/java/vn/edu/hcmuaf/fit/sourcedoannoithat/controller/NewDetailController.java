7605b0e835f09b0c6bf3b4e91b921a0dfe9bbbd2
nam <22130173@st.hcmuaf.edu.vn>
2025-04-09 11:17:37 +0700

    add NewDetailController

---
+++ src/main/java/vn/edu/hcmuaf/fit/sourcedoannoithat/controller/NewDetailController.java
@@ -0,0 +1,28 @@
+package vn.edu.hcmuaf.fit.sourcedoannoithat.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.sourcedoannoithat.dao.NewsDAO;
+import vn.edu.hcmuaf.fit.sourcedoannoithat.dao.model.News;
+
+import java.io.IOException;
+
+@WebServlet(name = "NewDetailController", value = "/NewDetailController")
+public class NewDetailController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        NewsDAO news = new NewsDAO();
+        News news1 = news.getNewsByID(id);
+
+        request.setAttribute("news", news1);
+        request.getRequestDispatcher("/mod/news-detail.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
